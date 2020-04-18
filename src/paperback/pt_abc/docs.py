753eb222e5fc3e4cf29f71512a12362788166e9a
Danil <danilunrandom@gmail.com>
2020-04-18 20:09:01 +0300

    feat(abc.docs): added ability to provide storage dir to docstring

--- src/paperback/pt_abc/docs.py
+++ src/paperback/pt_abc/docs.py
@@ -1,3 +1,4 @@
+from abc import ABCMeta
 from typing import ClassVar, Callable, Optional, NoReturn
 
 from fastapi import APIRouter
@@ -5,7 +6,7 @@ from fastapi import APIRouter
 from .base import Base
 
 
-class BaseDocs(Base):
+class BaseDocs(Base, metaclass=ABCMeta):
     """
     base class for all text modules of PaperText
 
@@ -15,12 +16,11 @@ class BaseDocs(Base):
         type of module
     DEFAULTS: Dict[str, int]
         python dict of default values for configuration
+    requires_dir: bool
+        describes if directory for storage will be provide to __init__ call
     """
 
     TYPE: ClassVar[str] = "DOCS"
-
-    # def __init__(self, cfg: Mapping[str, Any]):
-    # pass
 
     def create_router(
         self, token: Callable[[Optional[int], Optional[int]], Callable[[str], NoReturn]]
