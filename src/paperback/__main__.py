b1325a546562062fcfaf2131dbef06ea8b4fc8b4
Danil Kireev <danilunrandom@gmail.com>
2020-06-01 12:10:22 +0300

    feat: :wrench: more docs manipulationadded docs creation and cleaning and JB IDE support

--- src/paperback/__main__.py
+++ src/paperback/__main__.py
@@ -11,29 +11,39 @@ pyproject_path = source_path / "pyproject.toml"
 pyproject_path = pyproject_path.resolve()
 
 
-def lint():
-    call(f"python -m flakehell lint {src_path}".split(" "))
+class Scripts:
+    @staticmethod
+    def lint():
+        call(f"python -m flakehell lint {src_path}".split(" "))
 
+    @staticmethod
+    def fix_black():
+        call(f"python -m black {src_path} --config {pyproject_path}".split(" "))
 
-def fix_black():
-    call(f"python -m black {src_path} --config {pyproject_path}".split(" "))
+    @staticmethod
+    def fix_isort():
+        call(f"python -m isort -rc {src_path}".split(" "))
 
+    @staticmethod
+    def fix():
+        Scripts.fix_black()
+        Scripts.fix_isort()
 
-def fix_isort():
-    call(f"python -m isort -rc {src_path}".split(" "))
-
-
-def fix():
-    fix_black()
-    fix_isort()
-
-
-def docs():
-    call(
-        f"sphinx-build -b html {src_path / 'paperback_docs'} {source_path / 'docs'}".split(
-            " "
+    @staticmethod
+    def docs_build():
+        call(
+            f"sphinx-build -b html {src_path / 'paperback_docs'} {source_path / 'docs'}".split(
+                " "
+            )
         )
-    )
+
+    @staticmethod
+    def docs_clean():
+        call(
+            f"rm -rf {source_path / 'docs'}".split(
+                " "
+            )
+        )
 
 
 if __name__ == "__main__":
