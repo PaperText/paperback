5e2ae7d50145d2c3a45abf58c4a8cc8d2498b765
Danil Kireev <danilunrandom@gmail.com>
2020-06-24 23:49:03 +0300

    feat: added type hints

--- src/paperback_docs/conf.py
+++ src/paperback_docs/conf.py
@@ -1,40 +1,42 @@
+from typing import Any, Dict, List
+
 from recommonmark.transform import AutoStructify
 
 import paperback
 
-pt_version = paperback.__version__
 
 # project info
-project = "PaperText"
-copyright = "2020, Danil Kireev"
-author = "Danil Kireev"
-version, release = pt_version, pt_version
+project: str = "PaperText"
+copyright: str = "2020, Danil Kireev"
+author: str = "Danil Kireev"
+version: str = paperback.__version__,
+release: str = paperback.__version__
 
 # configuration
-master_doc = "index"
-source_suffix = [".rst", ".md"]
-extensions = [
+master_doc: str = "index"
+source_suffix: List[str] = [".rst", ".md"]
+extensions: List[str] = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.viewcode",
+    # "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
     "recommonmark",
     "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ["templates"]
+# templates_path: List[str] = ["templates"]
 
-# exclude_patterns = []
+# exclude_patterns: List[str] = []
 
 # HTML specific
-html_theme = "sphinx_typlog_theme"
-html_theme_options = {
+html_theme: str = "sphinx_typlog_theme"
+html_theme_options: Dict[str, str] = {
     "logo_name": "PaperBack",
     "description": "🗄API of 📎PaperText app",
     "github_user": "PaperText",
     "github_repo": "paperback",
 }
-html_sidebars = {
+html_sidebars: Dict[str, List[str]] = {
     "**": [
         "logo.html",
         "github.html",
@@ -43,21 +45,13 @@ html_sidebars = {
         "relations.html",
     ]
 }
-# html_static_path = ["static"]
+# html_static_path: List[str] = ["static"]
 
 # custom hooks
-source_link = "https://github.com/PaperText/paperback/blob/master/src"
+source_link: str = "https://github.com/PaperText/paperback/tree/master/src/paperback_docs"
 
 
-def linkcode_resolve(domain, info):
-    if domain != "py":
-        return None
-    module = info["module"].replace(".", "/")
-    filename = info["fullname"].replace(".", "/")
-    return "%s/%s/%s.py" % (source_link, module, filename)
-
-
-def setup(app):
+def setup(app: Any) -> Any:
     app.add_config_value(
         "recommonmark_config",
         {
@@ -66,7 +60,7 @@ def setup(app):
             "enable_math": False,
             "enable_inline_math": False,
             "enable_eval_rst": True,
-            # 'url_resolver': lambda url: github_doc_root + url,
+            "url_resolver": lambda url: source_link + url,
         },
         True,
     )
