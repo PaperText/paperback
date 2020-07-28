2d766b9b67d4d276f45fd8d86af9088c0c7abf2f
Danil Kireev <danilunrandom@gmail.com>
2020-07-28 16:46:58 +0300

    feat: added version reading from pyproject.toml file

--- src/paperback/__version__.py
+++ src/paperback/__version__.py
@@ -1,1 +1,11 @@
-__version__ = "0.2.0"
+from pathlib import Path
+
+from toml import load
+
+src_path = Path(__file__) / ".." / ".."
+source_path = src_path / ".."
+pyproject_path = source_path / "pyproject.toml"
+pyproject_path = pyproject_path.resolve()
+
+pyproject_toml = load(pyproject_path)
+__version__ = pyproject_toml["tool"]["poetry"]["version"]
