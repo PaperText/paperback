c37080b1d6d3dc1839a3ac9deb336e86ba6a705f
Random <unrndm@gmail.com>
2020-09-08 22:17:11 +0300

    fix(container): removed unused creation of docs relational database

--- src/docker/init_graph_db.sh
+++ src/docker/init_graph_db.sh
@@ -4,6 +4,5 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER paperback WITH PASSWORD '$paperback_dbuser_passwd';
     CREATE DATABASE auth_module;
-    CREATE DATABASE docs_module;
     GRANT ALL PRIVILEGES ON DATABASE auth_module, auth_module TO paperback;
 EOSQL
