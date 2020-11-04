fba2fc58b2226ad75af88ee4410f80ceafb48ee3
Random <unrndm@gmail.com>
2020-11-04 22:18:46 +0300

    feat: added missing docs and corpuses methods

--- src/paperback/abc/docs.py
+++ src/paperback/abc/docs.py
@@ -41,13 +41,27 @@ class BaseDocs(Base, metaclass=ABCMeta):
         storage_dir: Path,
         auth_module: BaseAuth
     ):
+        """
+        constructor of new module
+
+        Parameters
+        ----------
+        cfg : SimpleNamespace
+            configuration of module. Can be accessed with dot(cfg.config) or braces(cfg["config"])
+        storage_dir : Path
+            folder for storing temporary files
+        auth_module : BaseAuth
+            instance of module, which is responsible for authentification
+        """
         raise NotImplementedError
+
+    # docs
 
     @abstractmethod
     async def create_doc(
         self,
-        issuer_id: str,
-        issuer_type: str,
+        creator_id: str,
+        creator_type: str,
         doc_id: str,
         parent_corp_id: str,
         text: str,
@@ -57,7 +71,39 @@ class BaseDocs(Base, metaclass=ABCMeta):
         author: Optional[str] = None,
         created: Optional[datetime] = None,
         tags: Optional[List[str]] = None,
-    ):
+    ) -> Dict[str, Any]:
+        """
+        create doc with specified parameters
+
+        Parameters
+        ----------
+        creator_id : str
+            id of user, who issued request for creation
+        creator_type : str
+            type of user, who issued request for creation
+        doc_id: str
+            id of corpus to read
+        parent_corp_id: str, optional
+            new parent corpus, specified by it's id. default is None
+        text : str
+            text of the document
+        private: bool, optional
+            new private info. default is False
+        name: str, optional
+            new name of corpus. default is None
+        has_access: List[str], optional
+            list of people who have access to corpus. default is None
+        author: str, optional
+            author of the text. default is None
+        created: datetime, optional
+            date when text was created. default is None
+        tags: str, optional
+            optional list of tags. default is None
+
+        Returns
+        -------
+        Dict[str, Any]
+        """
         raise NotImplementedError
 
     @abstractmethod
@@ -68,8 +114,115 @@ class BaseDocs(Base, metaclass=ABCMeta):
         created_before: Optional[datetime] = None,
         created_after: Optional[datetime] = None,
         tags: Optional[List[str]] = None,
-    ):
+    ) -> List[Dict[str, Any]]:
+        """
+        search for document with specified parameters
+
+        Parameters
+        ----------
+        contains : str, optional
+            search string. default is None
+        author : str, optional
+            author of text. default is None
+        created_before : datetime, optional
+            date before which the text was created. default is None
+        created_after : datetime, optional
+            date after which the text was created. default is None
+        tags : List[str], optional
+            list of tags, that must be in text. default is None
+
+        Returns
+        -------
+        List[Dict[str, Any]]
+            List of documents
+        """
         raise NotImplementedError
+
+    @abstractmethod
+    async def read_doc(
+        self,
+        doc_id: str,
+    ) -> Dict[str, Any]:
+        """
+        read document with specified id
+
+        Parameters
+        ----------
+        doc_id : str
+            id of document to read info about
+
+        Returns
+        -------
+        Dict[str, Any]
+            info about document
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    async def update_doc(
+        self,
+        doc_id: str,
+        owner_id: Optional[str] = None,
+        owner_type: Optional[str] = None,
+        parent_corp_id: Optional[str] = None,
+        text: Optional[str] = None,
+        private: Optional[bool] = False,
+        name: Optional[str] = None,
+        has_access: Optional[List[str]] = None,
+        author: Optional[str] = None,
+        created: Optional[datetime] = None,
+        tags: Optional[List[str]] = None,
+    ) -> Dict[str, Any]:
+        """
+        create doc with specified parameters
+
+        Parameters
+        ----------
+        doc_id: str
+            id of corpus to read
+        owner_id : str, optional
+            id of new user, who owns this document. default is None
+        owner_type : str, optional
+            type of new user, who owns this document. default is None
+        parent_corp_id: str, optional
+            new parent corpus, specified by it's id. default is None
+        text : str, optional
+            new text of the document. default is None
+        private: bool, optional
+            new private info. default is False
+        name: str, optional
+            new name of corpus. default is None
+        has_access: List[str], optional
+            list of people who have access to corpus. default is None
+        author: str, optional
+            author of the text. default is None
+        created: datetime, optional
+            date when text was created. default is None
+        tags: str, optional
+            optional list of tags. default is None
+
+        Returns
+        -------
+        Dict[str, Any]
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    async def delete_doc(
+        self,
+        doc_id: str,
+    ):
+        """
+        delete document with specified id
+
+        Parameters
+        ----------
+        doc_id : str
+            id of document to delete
+        """
+        raise NotImplementedError
+
+    # corpuses
 
     @abstractmethod
     async def create_corp(
@@ -81,8 +234,34 @@ class BaseDocs(Base, metaclass=ABCMeta):
         parent_corp_id: Optional[str] = None,
         private: bool = False,
         has_access: Optional[List[str]] = None,
-        to_include=None,
+        to_include: Optional[List[str]] = None,
     ) -> Dict[str, Any]:
+        """
+        create corpus with specified parameters
+
+        Parameters
+        ----------
+        issuer_id : str
+            id of user, who issued request for creation
+        issuer_type : str
+            type of user, who issued request for creation
+        corp_id: str
+            id of corpus to read
+        name: str, optional
+            new name of corpus. default is None
+        parent_corp_id: str, optional
+            new parent corpus, specified by it's id. default is None
+        private: bool, optional
+            new private info. default is False
+        has_access: List[str], optional
+            list of people who have access to corpus. default is None
+        to_include: List[str], optional
+            list of sub entities. default is None
+
+        Returns
+        -------
+        Dict[str, Any]
+        """
         if to_include is None:
             to_include = []
         raise NotImplementedError
@@ -91,10 +270,84 @@ class BaseDocs(Base, metaclass=ABCMeta):
     async def read_corps(
         self,
         requester_id: str,
+    ) -> List[Dict[str, Any]]:
+        """
+        read corpuses of specified user
+
+        Parameters
+        ----------
+        requester_id: str
+            id of user to get corpuses of
+
+        Returns
+        -------
+        List[Dict[str, Any]]
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    async def read_corp(
+        self,
+        corp_id: str,
+    ) -> Dict[str, Any]:
+        """
+        read corpus with specified id
+
+        Parameters
+        ----------
+        corp_id: str
+            id of corpus to read
+
+        Returns
+        -------
+        Dict[str, Any]
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    async def update_corp(
+        self,
+        corp_id: str,
+        name: Optional[str] = None,
         parent_corp_id: Optional[str] = None,
         private: bool = False,
         has_access: Optional[List[str]] = None,
-    ):
+        to_include: Optional[List[str]] = None,
+    ) -> Dict[str, Any]:
+        """
+        update corpus with specified id and specified info
+
+        Parameters
+        ----------
+        corp_id: str
+            id of corpus to read
+        name: str, optional
+            new name of corpus. default is None
+        parent_corp_id: str, optional
+            new parent corpus, specified by it's id. default is None
+        private: bool, optional
+            new private info. default is False
+        has_access: List[str], optional
+            list of people who have access to corpus. default is None
+        to_include: List[str], optional
+            list of sub entities. default is None
+
+        Returns
+        -------
+        Dict[str, Any]
+        """
+        pass
+
+    @abstractmethod
+    async def delete_corp(self, corp_id: str):
+        """
+        delete corpus with specified id
+
+        Parameters
+        ----------
+        corp_id: str
+            id of corpus to delete
+        """
         raise NotImplementedError
 
     def create_router(self, token_tester: TokenTester) -> APIRouter:
@@ -110,8 +363,8 @@ class BaseDocs(Base, metaclass=ABCMeta):
             creates document with given id if it's not occupied
             """
             return await self.create_doc(
-                issuer_id=requester.user_id,
-                issuer_type="user",
+                creator_id=requester.user_id,
+                creator_type="user",
                 **dict(doc)
             )
 
@@ -128,7 +381,14 @@ class BaseDocs(Base, metaclass=ABCMeta):
             """
             returns list of all documents, accessible to user
             """
-            return []
+            raw_docs: List[Dict[str, Any]] = await self.read_docs(
+                contains=contains,
+                author=author,
+                created_before=created_before,
+                created_after=created_after,
+                tags=tags,
+            )
+            return ReadDocs(response=[ReadMinimalDoc(**doc) for doc in raw_docs])
 
         @router.get(
             "/docs/{doc_id}",
@@ -139,7 +399,7 @@ class BaseDocs(Base, metaclass=ABCMeta):
             """
             returns document with given id if it exists
             """
-            return None
+            return ReadDoc(**(await self.read_doc(doc_id)))
 
         @router.put(
             "/docs/{doc_id}", tags=["docs_module", "docs"],
@@ -148,14 +408,17 @@ class BaseDocs(Base, metaclass=ABCMeta):
             """
             updates document with given id if it exists
             """
-            return None
+            return await self.update_doc(
+                doc_id=doc_id,
+                **dict(doc),
+            )
 
         @router.delete("/docs/{doc_id}", tags=["docs_module", "docs"])
         async def delete_doc(doc_id: str):
             """
             deletes document with given id if it exists
             """
-            return None
+            return await self.delete_doc(doc_id)
 
         # corpus management
         @router.post("/corps", tags=["docs_module", "corps"])
@@ -181,7 +444,8 @@ class BaseDocs(Base, metaclass=ABCMeta):
             """
             returns list of all corpuses, accessible to user
             """
-            return await self.read_corps(requester_id=requester.user_id)
+            raw_corpuses: List[Dict[str, Any]] = await self.read_corps(requester_id=requester.user_id)
+            return ReadCorps(response=[ReadMinimalCorp(**corp) for corp in raw_corpuses])
 
         @router.get(
             "/corps/{corp_id}",
@@ -192,7 +456,7 @@ class BaseDocs(Base, metaclass=ABCMeta):
             """
             returns corpus with given id if it exists
             """
-            return None
+            return ReadCorp(**(await self.read_corp(corp_id=corp_id)))
 
         @router.put(
             "/corps/{corp_id}", tags=["docs_module", "corps"],
@@ -201,7 +465,10 @@ class BaseDocs(Base, metaclass=ABCMeta):
             """
             updates corpus with given id if it exists
             """
-            return None
+            return await self.update_corp(
+                corp_id=corp_id,
+                **dict(corp),
+            )
 
         @router.delete(
             "/corps/{corp_id}", tags=["docs_module", "corps"],
@@ -210,9 +477,16 @@ class BaseDocs(Base, metaclass=ABCMeta):
             """
             deletes corpus with given id if it exists
             """
-            return None
+            return await self.delete_corp(corp_id)
 
         # dictionary management
+        @router.post("/dicts", tags=["docs_module", "dict"])
+        async def create_dict(dict: CreateDict):
+            """
+            creates dictionaries with given id if it's not occupied
+            """
+            return None
+
         @router.get(
             "/dicts", tags=["docs_module", "dict"], response_model=ReadDicts,
         )
@@ -221,13 +495,6 @@ class BaseDocs(Base, metaclass=ABCMeta):
             returns list of all dictionaries, accessible to user
             """
             return []
-
-        @router.post("/dicts", tags=["docs_module", "dict"])
-        async def create_dict(dict: CreateDict):
-            """
-            creates dictionaries with given id if it's not occupied
-            """
-            return None
 
         @router.get(
             "/dicts/{dict_id}",
