59f38389b2009b864a2baa7ea282849fb5c245e2
unrndm <unrndm@gmail.com>
2021-06-30 18:49:15 +0300

    chore(container): :fire: removed ssh from docker container

--- src/container/entrypoint.sh
+++ src/container/entrypoint.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env sh
 
-eval "$(ssh-agent -s)"
-
-chmod 700 ~/.ssh
-
-ssh-keyscan gitlab.com >> ~/.ssh/known_hosts
-chmod 644 ~/.ssh/known_hosts
-
-if [ -f file ]; then
-    cp ~/.ssh/key ~/.ssh/private_key
-    chmod 400 ~/.ssh/private_key
-    ssh-add ~/.ssh/private_key
-fi
-
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
-
-
 modules=$(python3.8 -c '
 import os
 
