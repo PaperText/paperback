7eb5c26c8500a9d0611d1f7db606732112937ce4
unrndm <unrndm@gmail.com>
2021-07-05 01:47:21 +0300

    fix: fixed env var naming

--- src/container/init_psql_db.sh
+++ src/container/init_psql_db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER $psql_papertext_user WITH PASSWORD '$psql_papertext_password';
+    CREATE USER $psql_papertext_username WITH PASSWORD '$psql_papertext_password';
     CREATE DATABASE auth_module;
-    GRANT ALL PRIVILEGES ON DATABASE auth_module TO $psql_papertext_user;
+    GRANT ALL PRIVILEGES ON DATABASE auth_module TO $psql_papertext_username;
 EOSQL
