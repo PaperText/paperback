321edf94ca4ca325870113e35daafa3f53eec119
unrndm <unrndm@gmail.com>
2022-03-20 12:23:57 +0000

    chore: fixed inconsistencies between codespace and remote-container

--- src/paperback/docs/crud.py
+++ src/paperback/docs/crud.py
@@ -1,4 +1,6 @@
+from fastapi import HTTPException, status
 from py2neo import Transaction, Node
+
 from paperback.docs import schemas
 
 
@@ -10,15 +12,25 @@ def get_docs(tx: Transaction) -> list[schemas.Doc]:
 def create_doc(tx: Transaction, doc: schemas.DocCreate) -> schemas.Doc:
     doc_node = Node(
         "Document",
-        **(doc.dict()),
+        name=doc.name,
+        text=doc.text,
+        parent_corp_name= doc.parent_corp_name,
+        corp_name=doc.corp_name,
+        tags=doc.tags,
+        # **(doc.dict()),
     )
     tx.create(doc_node)
 
     if doc.parent_corp_name is not None:
         parent_corp = tx.graph.nodes.match("Corpus", name=doc.parent_corp_name).first()
         if parent_corp is None:
-            raise Exception("Corpus doesn't exist")
+            raise HTTPException(
+                status=status.HTTP_404_NOT_FOUND,
+                detail="Corpus doesn't exist",
+            )
 
-
-
+    raise HTTPException(
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Not implemented",
+            )
 
