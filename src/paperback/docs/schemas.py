5c6d4a280eb3a2dabbbaa1d30c587e0f2f14f394
unrndm <unrndm@gmail.com>
2022-03-29 13:22:19 +0000

    chore: applied fixers

--- src/paperback/docs/schemas.py
+++ src/paperback/docs/schemas.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
-from typing import Literal, TypedDict
-from enum import Enum
 
+from enum import Enum
+from typing import Literal, TypedDict
 
 from pydantic import BaseModel, EmailStr, Field, UUID4
-
 
 # doc
 # ---
@@ -89,29 +88,42 @@ class DocOrCorp(str, Enum):
     doc = "doc"
     corp = "corp"
 
+
 class DocsAndCorpuses(TypedDict):
     type: DocOrCorp
     name: str
 
 
 class AnalyzeLexics(BaseModel):
-    docs_and_corpuses: list[DocsAndCorpuses] = Field(..., description="list of documents and corpuses to analyze")
-    dicts: list[str] = Field(..., description="list of dictionary names to use during analysis")
+    docs_and_corpuses: list[DocsAndCorpuses] = Field(
+        ..., description="list of documents and corpuses to analyze"
+    )
+    dicts: list[str] = Field(
+        ..., description="list of dictionary names to use during analysis"
+    )
 
 
 class AnalyzePredicates(BaseModel):
-    docs_and_corpuses: list[DocsAndCorpuses] = Field(..., description="list of documents and corpuses to analyze")
+    docs_and_corpuses: list[DocsAndCorpuses] = Field(
+        ..., description="list of documents and corpuses to analyze"
+    )
     argument: str
     predicate: str
     role: str
 
 
 class AnalyzeStats(BaseModel):
-    docs_and_corpuses: list[DocsAndCorpuses] = Field(..., description="list of documents and corpuses to analyze")
+    docs_and_corpuses: list[DocsAndCorpuses] = Field(
+        ..., description="list of documents and corpuses to analyze"
+    )
     statistics: list[str]
 
 
 class AnalyzeCompare(BaseModel):
-    first_docs_and_corpuses: list[DocsAndCorpuses] = Field(..., description="first list of documents and corpuses to analyze")
-    second_docs_and_corpuses: list[DocsAndCorpuses] = Field(..., description="second list of documents and corpuses to analyze")
+    first_docs_and_corpuses: list[DocsAndCorpuses] = Field(
+        ..., description="first list of documents and corpuses to analyze"
+    )
+    second_docs_and_corpuses: list[DocsAndCorpuses] = Field(
+        ..., description="second list of documents and corpuses to analyze"
+    )
     statistics: list[str]
